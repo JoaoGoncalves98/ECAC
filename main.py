522fd017962b70698d18e4d0daf75e494b88624e
JoaoGoncalves98 <up201604245@fe.up.pt>
2020-10-13 17:00:21 +0100

    Organizar em funções + accuracy de usar 93-95 para prever 96

--- main.py
+++ main.py
@@ -3,39 +3,64 @@ import numpy as np
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import csv
 
-# Devemos dividir os dados do train e usa-los tb para test e ver qual é o melhor a partir daí
-# e depois usamos o com maior eficiencia no ficheiro de test.
-os.chdir('data')
-heart = pd.read_csv('loan_train.csv', sep=';', header=0)
-heart.head()
+def get_train_values():
+    os.chdir('data')
+    heart = pd.read_csv('loan_train.csv', sep=';', header=0)
+    heart.head()
+    return heart
+    
+def get_test_values(values):
+    heart = pd.read_csv('loan_test.csv', sep=';', header=0)
+    heart.head()
+    train_y = values.iloc[:,6]
+    train_x = values.iloc[:,:6]
+    test_y = heart.iloc[:,6]
+    test_x = heart.iloc[:,:6]
+    loan_id = list(heart.iloc[:,0])
+    return (train_x, train_y, test_x, test_y, loan_id)
 
-train_y = heart.iloc[:,6]
-train_x = heart.iloc[:,:6]
+def train_split_random(values):
+    values_y = values.iloc[:,6]
+    values_x = values.iloc[:,:6]
+    train_y, test_y = train_test_split(values_y, test_size=0.25, random_state=0, shuffle=True)
+    train_x, test_x = train_test_split(values_x, test_size=0.25, random_state=0, shuffle=True)
+    return(train_x, train_y, test_x, test_y)
 
-heart = pd.read_csv('loan_test.csv', sep=';', header=0)
-heart.head()
+def train_predict_96(values):
+    values_train=values[(values['date'] > 930000) & (values['date'] < 960000)]
+    values_test=values[values['date'] > 960000]
+    train_y = values_train.iloc[:,6]
+    train_x = values_train.iloc[:,:6]
+    test_y = values_test.iloc[:,6]
+    test_x = values_test.iloc[:,:6]
+    return(train_x, train_y, test_x, test_y)
 
-test_y = heart.iloc[:,6]
-test_x = heart.iloc[:,:6]
+def apply_gaussian(train_x, train_y, test_x):
+    gnb = GaussianNB()
+    model = gnb.fit(train_x, train_y)
+    return gnb.predict(test_x)
 
-# Aqui aplicamos um algoritmo para calcular os valores e para a % de acertamento
-gnb = GaussianNB()
-model = gnb.fit(train_x, train_y)
-preds = gnb.predict(test_x)
+def create_file(loan_id,results):
+    os.chdir('..')
+    f=open('submission.csv','w')
+    f.write("Id,Predicted")
+    for i,j in zip(loan_id,results):
+        f.write("\n"+str(i)+","+str(j))
+    f.close()
 
+# _____________________________________________________________________________________________________________
 
+values = get_train_values()
 
-# Criar o ficheiro csv
-loan_id = list(heart.iloc[:,0])
-results = list(preds)
+# train_x, train_y, test_x, test_y = train_split_random(values)
+train_x, train_y, test_x, test_y = train_predict_96(values)
+# train_x, train_y, test_x, test_y, loan_id = get_test_values(values)
 
-print(metrics.auc(preds, list(test_y)))
+preds = list(apply_gaussian(train_x, train_y, test_x))
 
-os.chdir('..')
-f=open('submission.csv','w')
-f.write("Id,Predicted")
-for i,j in zip(loan_id,results):
-    f.write("\n"+str(i)+","+str(j))
-f.close()
+print(accuracy_score(test_y, preds))
+# create_file(loan_id,preds)
